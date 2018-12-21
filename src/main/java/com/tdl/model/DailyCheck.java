981e0fed60bdf1c5ad7339b40bc5970e535b9f9a
Delf-Lee <ttjs2008@naver.com>
2018-12-21 15:41:25 +0900

    Add DailyCheck class

---
+++ src/main/java/com/tdl/model/DailyCheck.java
@@ -0,0 +1,24 @@
+package com.tdl.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+public class DailyCheck {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private Date planedTime;
+    private boolean isDone;
+
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "todo_id")
+    private Todo todo;
+}
--- src/main/java/com/tdl/model/Todo.java
+++ src/main/java/com/tdl/model/Todo.java
@@ -6,7 +6,6 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Date;
-import java.util.List;
 
 @Entity
 @Getter
