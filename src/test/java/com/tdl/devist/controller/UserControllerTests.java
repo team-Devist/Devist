ff9d913b698a372bc4ad8b1e4099e14d915d3d02
cjh5414 <cjh5414@gmail.com>
2018-12-25 20:02:36 +0900

    Add csrf code and test creating user

--- src/test/java/com/tdl/devist/controller/UserControllerTests.java
+++ src/test/java/com/tdl/devist/controller/UserControllerTests.java
@@ -1,6 +1,7 @@
 package com.tdl.devist.controller;
 
 
+import com.tdl.devist.DevistApplication;
 import com.tdl.devist.model.User;
 import com.tdl.devist.repository.UserRepository;
 import org.junit.Assert;
@@ -8,22 +9,31 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import javax.transaction.Transactional;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Profile("dev")
+@ContextConfiguration
+@WebAppConfiguration
 public class UserControllerTests {
+    @Autowired
+    private WebApplicationContext context;
     private MockMvc mockMvc;
 
     @MockBean
@@ -34,20 +44,31 @@ public class UserControllerTests {
 
     @Before
     public void setup() {
-        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(this.context)
+                .apply(springSecurity())
+                .build();
     }
 
     @Test
+    @Transactional
     public void testCreateUser() throws Exception {
         mockMvc.perform(post("/signup")
                 .param("username", "user1")
                 .param("password", "user1234")
-                .param("name", "name1"))
-                .andExpect(status().isOk())
-                .andExpect(redirectedUrl("/"));
+                .param("name", "name1")
+                .with(csrf()))
+                .andExpect(status().isOk());
+
+
+        userRepository.flush();
+        Assert.assertEquals(3, userRepository.findAll().size());
+        // Todo: test 통과 시키기. /signup 성공 이후 생성한 User를 userRepository를 통해서 가져올 수 없음.
 
         User user = userRepository.getOne("user1");
+        Assert.assertEquals(1, user.getEnabled());
+        Assert.assertEquals(1, user.getEnabled());
+        Assert.assertEquals(100.00, user.getDoneRate(), 00.01);
         Assert.assertEquals("name1", user.getName());
     }
-
 }
