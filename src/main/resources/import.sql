1da966127651fe327f5a878831cdfdcaa7032444
Delf-Lee <ttjs2008@naver.com>
2019-02-21 20:07:17 +0900

    Add test data via to import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -9,12 +9,16 @@ insert into todos (todo_id, title, description, created_time, done_rate, usernam
 insert into todos (todo_id, title, description, created_time, done_rate, username) values (3, '블로그 쓰기', '수요일 마다', '2019-01-02 11:20:14.000', 0,'cjh5414');
 insert into todos (todo_id, title, description, created_time, done_rate, username) values (4, '완료된 할 일', '매일 하는 일인데 테스트 할라고 완료 시킴', '2019-01-02 11:20:14.000', 0, 'cjh5414');
 insert into todos (todo_id, title, description, created_time, done_rate, username) values (5, 'Daily check 많이 가지고 있는 할 일', 'Daily Check 4에 1개만 완료.', '2018-01-02 11:20:14.000', 25.00, 'cjh5414');
+insert into todos (todo_id, title, description, created_time, done_rate, username) values (6, '주4회 중 3회 완료', '주4회 중 3회 완료.', '2018-01-02 11:20:14.000', 25.00, 'cjh5414');
+insert into todos (todo_id, title, description, created_time, done_rate, username) values (7, '주4회 중 4회 완료', '주4회 중 4회 완료.', '2018-01-02 11:20:14.000', 25.00, 'cjh5414');
 
 insert into repeat_day (repeat_type, todo_id, days_of_week) values ('FixedRepeatDay', 1, 127);
 insert into repeat_day (repeat_type, todo_id, days_of_week) values ('FixedRepeatDay', 2, 127);
 insert into repeat_day (repeat_type, todo_id, days_of_week) values ('FixedRepeatDay', 3, 127);
 insert into repeat_day (repeat_type, todo_id, days_of_week) values ('FixedRepeatDay', 4, 127);
 insert into repeat_day (repeat_type, todo_id, days_of_week) values ('FixedRepeatDay', 5, 127);
+insert into repeat_day (repeat_type, todo_id, doing_count, weeks_count) values ('FlexibleRepeatDay', 6, 3, 4);
+insert into repeat_day (repeat_type, todo_id, doing_count, weeks_count) values ('FlexibleRepeatDay', 7, 7, 4);
 
 insert into daily_checks (daily_check_id, planed_date, is_done, todo_id) values (1, '2018-12-22', 1, 3);
 insert into daily_checks (daily_check_id, planed_date, is_done, todo_id) values (2, '2018-12-20', 0, 4);
